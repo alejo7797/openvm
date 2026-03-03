a4fa612b80e12db36b307b1f6620460cdd23f42b
Alex Epelde <alex@epelde.net>
2026-03-03 18:49:23 -0500

    nix: add solc to devShell

--- flake.nix
+++ flake.nix
@@ -22,17 +22,45 @@
           ...
         }:
         let
-          toolchain = inputs'.fenix.packages.fromToolchainFile {
+          inherit (pkgs)
+            autoPatchelfHook
+            stdenv
+            fetchurl
+            ;
+
+          rustToolchain = inputs'.fenix.packages.fromToolchainFile {
             dir = ./.;
             sha256 = "sha256-SJwZ8g0zF2WrKDVmHrVG3pD2RGoQeo24MEXnNx5FyuI=";
+          };
+
+          solc_0_8_19 = stdenv.mkDerivation {
+            pname = "solc";
+            version = "0.8.19";
+            src = fetchurl {
+              url = "https://binaries.soliditylang.org/linux-amd64/solc-linux-amd64-v0.8.19+commit.7dd6d404";
+              hash = "sha256-elwdPcmo66Yrsuw3GSyReK5f6KVKVuVXP9PJwXzZ60g=";
+            };
+            dontUnpack = true;
+            nativeBuildInputs = [
+              autoPatchelfHook
+            ];
+            installPhase = ''
+              runHook preInstall
+              install -Dm755 $src $out/bin/solc
+              runHook postInstall
+            '';
           };
         in
         {
           _module.args = {
             pkgs = inputs'.nixpkgs.legacyPackages;
           };
+
           devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [ toolchain ];
+            nativeBuildInputs = [
+              solc_0_8_19
+              rustToolchain
+            ];
           };
         };
     };
