fa0572aa0a5f7a65c85f37d7d226dbbaa2b85f79
Alex Epelde <alex@epelde.net>
2026-03-04 02:44:50 -0500

    nix: add package output

--- flake.nix
+++ flake.nix
@@ -13,55 +13,95 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem =
-        {
-          inputs',
-          pkgs,
-          ...
-        }:
-        let
-          inherit (pkgs)
-            autoPatchelfHook
-            stdenv
-            fetchurl
-            ;
-
-          rustToolchain = inputs'.fenix.packages.fromToolchainFile {
-            dir = ./.;
-            sha256 = "sha256-SJwZ8g0zF2WrKDVmHrVG3pD2RGoQeo24MEXnNx5FyuI=";
-          };
-
-          solc_0_8_19 = stdenv.mkDerivation {
-            pname = "solc";
-            version = "0.8.19";
-            src = fetchurl {
-              url = "https://binaries.soliditylang.org/linux-amd64/solc-linux-amd64-v0.8.19+commit.7dd6d404";
-              hash = "sha256-elwdPcmo66Yrsuw3GSyReK5f6KVKVuVXP9PJwXzZ60g=";
-            };
-            dontUnpack = true;
-            nativeBuildInputs = [
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = [ "x86_64-linux" ];
+        perSystem =
+          {
+            inputs',
+            pkgs,
+            self',
+            ...
+          }:
+          let
+            inherit (pkgs)
               autoPatchelfHook
-            ];
-            installPhase = ''
-              runHook preInstall
-              install -Dm755 $src $out/bin/solc
-              runHook postInstall
-            '';
-          };
-        in
-        {
-          _module.args = {
-            pkgs = inputs'.nixpkgs.legacyPackages;
-          };
+              fetchurl
+              makeRustPlatform
+              stdenv
+              ;
 
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [
-              solc_0_8_19
-              rustToolchain
-            ];
+            rustToolchain = inputs'.fenix.packages.fromToolchainFile {
+              dir = ./.;
+              sha256 = "sha256-SJwZ8g0zF2WrKDVmHrVG3pD2RGoQeo24MEXnNx5FyuI=";
+            };
+
+            rustPlatform = makeRustPlatform {
+              cargo = rustToolchain;
+              rustc = rustToolchain;
+            };
+
+            solc_0_8_19 = stdenv.mkDerivation {
+              pname = "solc";
+              version = "0.8.19";
+              src = fetchurl {
+                url = "https://binaries.soliditylang.org/linux-amd64/solc-linux-amd64-v0.8.19+commit.7dd6d404";
+                hash = "sha256-elwdPcmo66Yrsuw3GSyReK5f6KVKVuVXP9PJwXzZ60g=";
+              };
+              dontUnpack = true;
+              nativeBuildInputs = [
+                autoPatchelfHook
+              ];
+              installPhase = ''
+                runHook preInstall
+                install -Dm755 $src $out/bin/solc
+                runHook postInstall
+              '';
+            };
+          in
+          {
+            _module.args = {
+              pkgs = inputs'.nixpkgs.legacyPackages;
+            };
+
+            devShells.default = pkgs.mkShell {
+              nativeBuildInputs = [
+                solc_0_8_19
+                rustToolchain
+              ];
+            };
+
+            packages = {
+
+              openvm = rustPlatform.buildRustPackage {
+                pname = "openvm";
+                version = "2.0.0-beta.1";
+                src = ./.;
+                cargoLock = {
+                  lockFile = ./Cargo.lock;
+                  allowBuiltinFetchGit = true;
+                };
+                cargoBuildFlags = [
+                  "--package"
+                  "cargo-openvm"
+                ];
+                doCheck = false;
+                meta = {
+                  description = "Performant and modular zkVM framework built for customization and extensibility";
+                  homepage = "https://openvm.dev/";
+                  license = with lib.licenses; [
+                    asl20
+                    mit
+                  ];
+                  mainProgram = "cargo-openvm";
+                };
+              };
+
+              default = self'.packages.openvm;
+
+            };
           };
-        };
-    };
+      }
+    );
 }
